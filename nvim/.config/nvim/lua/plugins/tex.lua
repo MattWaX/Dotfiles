12c39b6725129d18505076013d21e62dc9529fbc
MattWaX <mattwexx2929@gmail.com>
2025-04-25 17:07:17 +0200

    nvim: vimtex output moved to cache

--- nvim/.config/nvim/lua/plugins/tex.lua
+++ nvim/.config/nvim/lua/plugins/tex.lua
@@ -7,8 +7,8 @@ return {
             vim.g.vimtex_view_method = "zathura"
             vim.g.vimtex_view_forward_search_on_start = false
             vim.g.vimtex_compiler_latexmk = {
-                aux_dir = "/home/MattWaX/.texfiles/",
-                out_dir = "/home/MattWaX/.texfiles/",
+                aux_dir = "/home/MattWaX/.cache/texfiles/",
+                out_dir = "/home/MattWaX/.cache/texfiles/",
             }
         end,
     },
