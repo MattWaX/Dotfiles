32b11875aaf8b04a35d695c0013de89150f0fd30
MattWaX <mattwexx2929@gmail.com>
2025-04-28 00:40:16 +0200

    nvim: show bufferline only i there is more than one buffer

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -9,6 +9,7 @@ return {
                 show_close_icon = true,
                 show_buffer_close_icons = true,
                 truncate_names = false,
+                always_show_bufferline = false,
                 indicator = { style = 'underline' },
                 diagnostics = 'nvim_lsp',
             },
