57900081d2725c78d3e6462dba2731acc597d109
MattWaX <mattwexx2929@gmail.com>
2025-04-27 16:00:08 +0200

    nvim: sql completion and dadbod plugins refactoring

--- nvim/.config/nvim/lua/plugins/dadbod.lua
+++ nvim/.config/nvim/lua/plugins/dadbod.lua
@@ -1,43 +1,19 @@
 return {
     -- DB plugins
-    {
-        "tpope/vim-dadbod",
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+        { "tpope/vim-dadbod", lazy = true },
+        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
     },
-    {
-        "kristijanhusak/vim-dadbod-ui",
-        dependencies = {
-            { "tpope/vim-dadbod", lazy = true },
-            { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-        },
-        cmd = {
-            "DBUI",
-            "DBUIToggle",
-            "DBUIAddConnection",
-            "DBUIFindBuffer",
-        },
-        init = function()
-            -- Your DBUI configuration
-            vim.g.db_ui_use_nerd_fonts = 1
-            vim.g.db_ui_execute_on_save = 0
-        end,
+    cmd = {
+        "DBUI",
+        "DBUIToggle",
+        "DBUIAddConnection",
+        "DBUIFindBuffer",
     },
---     {
---         "kristijanhusak/vim-dadbod-completion",
---         ft = { "sql", "mysql", "plsql" },
---         lazy = true,
---         dependencies = {
---             { "tpope/vim-dadbod" },
---             { "kristijanhusak/vim-dadbod-completion" },
---         },
---         cmd = {
---             "DBUI",
---             "DBUIToggle",
---             "DBUIAddConnection",
---             "DBUIFindBuffer",
---         },
---         init = function()
---             -- Your DBUI configuration
---             vim.g.db_ui_use_nerd_fonts = 1
---         end,
---     },
--- }
+    init = function()
+        -- Your DBUI configuration
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.db_ui_execute_on_save = 0
+    end,
+}
