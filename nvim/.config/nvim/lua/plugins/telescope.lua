cac2f3d4487ac9b4a9b6dd0be1ba04de8da9cca2
MattWaX <mattwexx2929@gmail.com>
2025-05-01 22:07:41 +0200

    nvim: added snacks plugin to substitute telescope and have image integration

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,26 @@
 return {
     {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-treesitter/nvim-treesitter" },
-        cmd = "Telescope",
-        opts = {
-            defaults = {
-                prompt_prefix = "   ",
-                selection_caret = " ",
-                entry_prefix = " ",
-                sorting_strategy = "ascending",
-                layout_config = {
-                    horizontal = {
-                        prompt_position = "top",
-                        preview_width = 0.55,
-                    },
-                    width = 0.87,
-                    height = 0.80,
-                },
-            },
-
-            extensions_list = { "themes", "terms" },
-            extensions = {},
-        },
+        -- "nvim-telescope/telescope.nvim",
+        -- dependencies = { "nvim-treesitter/nvim-treesitter" },
+        -- cmd = "Telescope",
+        -- opts = {
+        --     defaults = {
+        --         prompt_prefix = "   ",
+        --         selection_caret = " ",
+        --         entry_prefix = " ",
+        --         sorting_strategy = "ascending",
+        --         layout_config = {
+        --             horizontal = {
+        --                 prompt_position = "top",
+        --                 preview_width = 0.55,
+        --             },
+        --             width = 0.87,
+        --             height = 0.80,
+        --         },
+        --     },
+        --
+        --     extensions_list = { "themes", "terms" },
+        --     extensions = {},
+        -- },
     },
 }
