7450264b5a9eb4d6bbf2a84500af98ae60284cde
MattWaX <mattwexx2929@gmail.com>
2025-05-07 19:11:35 +0200

    nvim: mason tools

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,57 +1,68 @@
 return {
-    "williamboman/mason.nvim",
-    cmd = { "Mason", "MasonInstall", "MasonUpdate" },
-    opts = {
-        PATH = "skip",
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            PATH = "skip",
 
-        ensure_installed = {
-            -- lua
-            "lua-language-server",
-            "stylua",
-            -- shell
-            "bash-language-server",
-            "bash-debug-adapter",
-            "shfmt",
-            -- web
-            "css-lsp",
-            "html-lsp",
-            -- c/cpp
-            "clangd",
-            "clang-format",
-            -- rust
-            "rust-analyzer",
-            "taplo",
-            -- c, cpp, rust debugger
-            "codelldb",
-            -- asm
-            "asm-lsp",
-            "asmfmt",
-            -- java
-            "jdtls",
-            "java-debug-adapter",
-            -- php
-            "phpactor",
-            "php-debug-adapter",
-            -- tex
-            "texlab",
-            "bibtex-tidy",
-            -- R
-            "r-languageserver",
-            "air",
-            -- XML
-            "lemminx",
-            -- treesitter
-            "tree-sitter-cli",
+            ui = {
+                icons = {
+                    package_pending = " ",
+                    package_installed = " ",
+                    package_uninstalled = " ",
+                },
+            },
+
+            max_concurrent_installers = 10,
         },
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        opts = {
+            ensure_installed = {
+                -- lua
+                "lua-language-server",
+                "stylua",
+                -- shell
+                "bash-language-server",
+                "bash-debug-adapter",
+                "shfmt",
+                -- web
+                "css-lsp",
+                "html-lsp",
+                -- c/cpp
+                "clangd",
+                "clang-format",
+                -- rust
+                "rust-analyzer",
+                "taplo",
+                -- c, cpp, rust debugger
+                "codelldb",
+                -- asm
+                "asm-lsp",
+                "asmfmt",
+                -- java
+                "jdtls",
+                "java-debug-adapter",
+                -- php
+                "phpactor",
+                "php-debug-adapter",
+                -- tex
+                "texlab",
+                "bibtex-tidy",
+                -- R
+                "r-languageserver",
+                "air",
+                -- XML
+                "lemminx",
+                -- treesitter
+                "tree-sitter-cli",
+            },
 
-        ui = {
-            icons = {
-                package_pending = " ",
-                package_installed = " ",
-                package_uninstalled = " ",
+            integrations = {
+                ["mason-lspconfig"] = true,
+                ["mason-null-ls"] = true,
+                ["mason-nvim-dap"] = true,
             },
         },
-
-        max_concurrent_installers = 10,
     },
 }
