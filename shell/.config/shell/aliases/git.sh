ff7e46d56f67f2b79b2ad02555288e1a93cc145a
MattWaX <mattwexx2929@gmail.com>
2025-05-10 14:27:52 +0200

    shell: git push notes alias

--- shell/.config/shell/aliases/git.sh
+++ shell/.config/shell/aliases/git.sh
@@ -1,7 +1,9 @@
-git_push_date_commit() {
-    git add *;
-    git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)";
-    git push;
+git_push_notes() {
+    git add "$HOME/Documents/Appunti/*";
+    if [ $? ]; then
+        git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)";
+        git push;
+    fi
 }
 
-alias gitdate='git_push_date_commit'
+alias notes_push='git_push_notes'
