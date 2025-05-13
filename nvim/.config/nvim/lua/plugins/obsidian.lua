d651e18cb6b05af0e2c01e746a50be4c529b4fb1
MattWaX <mattwexx2929@gmail.com>
2025-05-14 00:04:00 +0200

    nvim: better obsidian.nvim config

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -89,6 +89,24 @@ return {
 
             preferred_link_style = "markdown",
 
+            ui = {
+                enable = true, -- set to false to disable all additional syntax features
+                update_debounce = 200, -- update delay after a text change (in milliseconds)
+                max_file_length = 5000, -- disable UI features for files with more than this many lines
+                -- Define how various check-boxes are displayed
+                checkboxes = {
+                    -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+                    [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+                    ["x"] = { char = "", hl_group = "ObsidianDone" },
+                    [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+                    ["+"] = { char = "", hl_group = "ObsidianRightArrow" },
+                    ["-"] = { char = "", hl_group = "ObsidianTilde" },
+                    ["~"] = { char = "", hl_group = "ObsidianTilde" },
+                    ["!"] = { char = "", hl_group = "ObsidianImportant" },
+
+                },
+            },
+
             disable_frontmatter = true,
 
             note_frontmatter_func = function(note)
