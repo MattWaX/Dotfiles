5051e9373fd3cf55f98534654b167e2cf7ac6ca4
MattWaX <mattwexx2929@gmail.com>
2025-05-17 15:16:08 +0200

    scripts: better songinfo

--- scripts/.scripts/songinfo.sh
+++ scripts/.scripts/songinfo.sh
@@ -5,5 +5,10 @@ filename="$(mpc --format "$music_dir"/%file% current)"
 previewname="$music_dir/$(mpc --format %artist% current)/$(mpc --format %album% current)/cover.png"
 
 [ -e "$previewname" ] || ffmpeg -y -i "$filename" -an -vf scale=128:128 "$previewname" > /dev/null 2>&1
+previus_notification=$(cat /tmp/last-song-notification-id)
+if [ -z $previus_notification ]; then
 
-notify-send -r 27072 "Now Playing" "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname"
+    notify-send "Now Playing" "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname" -p > /tmp/last-song-notification-id
+else
+    notify-send -r $previus_notification "Now Playing" "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname" -p > /tmp/last-song-notification-id
+fi
