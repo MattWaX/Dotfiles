14e2479773e47af3a92ec8158ac17b816523a688
MattWaX <mattwexx2929@gmail.com>
2025-05-20 11:23:28 +0200

    nvim: automatic latex nabla ascii formulas removed

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -4,23 +4,27 @@ local opt = vim.o
 
 opt.spell = true
 
-local nabla = require "nabla"
-
-vim.api.nvim_create_autocmd({ "BufWrite" }, {
-    callback = function()
-        if nabla.is_virt_enabled() then
-            nabla.enable_virt()
-        end
-    end,
-})
-nabla.enable_virt()
-vim.api.nvim_create_autocmd({ "InsertEnter" }, {
-    callback = function()
-        nabla.disable_virt()
-    end,
-})
-vim.api.nvim_create_autocmd({ "InsertLeave" }, {
-    callback = function()
-        nabla.enable_virt()
-    end,
-})
+-- local nabla = require "nabla"
+--
+-- vim.api.nvim_create_autocmd({ "BufWrite" }, {
+--     callback = function()
+--         if nabla.is_virt_enabled() then
+--             nabla.enable_virt()
+--         end
+--     end,
+-- })
+-- nabla.enable_virt()
+-- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+--     callback = function()
+--         if nabla.is_virt_enabled() then
+--             nabla.disable_virt()
+--         end
+--     end,
+-- })
+-- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+--     callback = function()
+--         if nabla.is_virt_enabled() then
+--             nabla.enable_virt()
+--         end
+--     end,
+-- })
