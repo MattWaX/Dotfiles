fe24785ad090b00ecbca1b1d424200d26ac60825
MattWaX <mattwexx2929@gmail.com>
2025-05-21 11:22:19 +0200

    shell: tranformed the aliasses in a makefile

--- shell/.config/shell/aliases/git.sh
+++
@@ -1,9 +0,0 @@
-git_push_notes() {
-    git add "$HOME/Documents/Appunti/*";
-    if [ $? ]; then
-        git commit -m "$(date +%Y-%m-%d\ %H:%M:%S)";
-        git push;
-    fi
-}
-
-alias notes_push='git_push_notes'
