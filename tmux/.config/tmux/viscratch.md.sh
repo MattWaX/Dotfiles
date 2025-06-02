35ad9b4395097d10bf00b0081a751d0a53715cb1
MattWaX <mattwexx2929@gmail.com>
2025-06-02 18:27:43 +0200

    tmux: todo popup now opens the dump note

--- tmux/.config/tmux/viscratch.md.sh
+++ tmux/.config/tmux/viscratch.md.sh
@@ -2,4 +2,12 @@ width=${2:-30%}
 height=${2:-70%}
 x=${2:-100%}
 
-tmux popup -x"$x" -yS -w"$width" -h"$height" -E "nvim ~/.todo.md"
+dump="$HOME/Documents/Appunti/_Note/Dump.md"
+
+if [ -e "$dump" ]; then
+    ln -s "$dump" ~/.dump.md
+    tmux popup -x"$x" -yS -w"$width" -h"$height" -E "nvim ~/.dump.md -c \"cd ${dump%/*/*}\""
+else
+    tmux popup -x"$x" -yS -w"$width" -h"$height" -E "nvim ~/.dump.md"
+fi
+
