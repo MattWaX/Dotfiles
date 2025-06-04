5473872ed6f994b39edd20caa0dba31f8532960e
MattWaX <mattwexx2929@gmail.com>
2025-06-04 18:31:40 +0200

    nvim: colorscheme fixes

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -1,3 +1,10 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "CursorLineSign", { link =  "Normal" })
+        vim.api.nvim_set_hl(0, "SignColumn", { link =  "Normal" })
+    end,
+})
+
 return {
     { "vinitkumar/oscura-vim", lazy = false },
     { "orthecreedence/void.vim", lazy = false },
