9523d745641e5037b3604e7a2a47395b3eba0958
MattWaX <mattwexx2929@gmail.com>
2025-06-25 02:14:26 +0200

    nvim: preset macros for markdown files for quick bold/italic

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -8,30 +8,9 @@ opt.spell = true
 opt.textwidth = 79
 opt.colorcolumn = "+1"
 
-map("n", "<F5>", ":Make!<CR>", { desc = "Silent Make"})
-map("n", "<F6>", ":Make<CR>", { desc = "Make"})
+local setreg = vim.fn.setreg
+setreg("b", "ciw****hhpb") -- bold macro
+setreg("i", "ciw**hpb") -- italic macro
 
--- local nabla = require "nabla"
---
--- vim.api.nvim_create_autocmd({ "BufWrite" }, {
---     callback = function()
---         if nabla.is_virt_enabled() then
---             nabla.enable_virt()
---         end
---     end,
--- })
--- nabla.enable_virt()
--- vim.api.nvim_create_autocmd({ "InsertEnter" }, {
---     callback = function()
---         if nabla.is_virt_enabled() then
---             nabla.disable_virt()
---         end
---     end,
--- })
--- vim.api.nvim_create_autocmd({ "InsertLeave" }, {
---     callback = function()
---         if nabla.is_virt_enabled() then
---             nabla.enable_virt()
---         end
---     end,
--- })
+map("n", "<F5>", ":Make!<CR>", { desc = "Silent Make" })
+map("n", "<F6>", ":Make<CR>", { desc = "Make" })
