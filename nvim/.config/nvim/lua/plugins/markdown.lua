e3847befb7e0d900206f09987ebd87f884edb9b1
MattWaX <mattwexx2929@gmail.com>
2025-10-13 23:44:27 +0200

    nvim: better markdown integration

---
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,280 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+        vim.api.nvim_set_hl(0, "RenderMarkdownInlineHighlight", { bg = "#75662E" })
+    end,
+})
+
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    config = function()
+        require("render-markdown").setup {
+            enabled = true,
+            preset = "Obsidian",
+            completions = {
+                blink = { enabled = true },
+                lsp = { enabled = true },
+                filter = {
+                    checkbox = function()
+                        return true
+                    end,
+                },
+            },
+            heading = {
+                enabled = true,
+                atx = true,
+                setext = true,
+                sign = true,
+                icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+                position = "overlay",
+                signs = { "#>" },
+                width = "block",
+                border = true,
+                above = " ",
+                below = "=",
+            },
+            dash = {
+                enabled = true,
+                icon = "─",
+                width = 79,
+                -- Amount of margin to add to the left of dash.
+                -- If a float < 1 is provided it is treated as a percentage of available window space.
+                left_margin = 0,
+                -- Highlight for the whole line generated from the icon.
+                highlight = "RenderMarkdownDash",
+            },
+            bullet = {
+                enabled = true,
+                icons = { "󰧞" },
+            },
+            checkbox = {
+                enabled = true,
+                unchecked = {
+                    icon = "󰄱 ",
+                    highlight = "RenderMarkdownUnchecked",
+                    scope_highlight = nil,
+                },
+                checked = {
+                    icon = " ",
+                    highlight = "RenderMarkdownChecked",
+                    scope_highlight = nil,
+                },
+                custom = {
+                    inprogress = {
+                        raw = "[>]",
+                        rendered = "󰛂 ",
+                        highlight = "RenderMarkdownChecked",
+                        scope_highlight = nil,
+                    },
+                    tilde = {
+                        raw = "[~]",
+                        rendered = "󰅗 ",
+                        highlight = "RenderMarkdownChecked",
+                        scope_highlight = nil,
+                    },
+                    important = {
+                        raw = "[!]",
+                        rendered = "󰀧 ",
+                        highlight = "RenderMarkdownChecked",
+                        scope_highlight = nil,
+                    },
+                },
+            },
+            inline_highlight = {
+                enabled = true,
+                render_modes = true,
+                highlight = "RenderMarkdownInlineHighlight",
+            },
+            callout = {
+                esempio = {
+                    raw = "[!ESEMPIO]",
+                    rendered = "󰉹 Esempio",
+                    highlight = "RenderMarkdownHint",
+                    category = "obsidian",
+                },
+                definizione = {
+                    raw = "[!DEF]",
+                    rendered = " Definizione",
+                    highlight = "RenderMarkdownWarn",
+                    category = "obsidian",
+                },
+                teorema = {
+                    raw = "[!TEOREMA]",
+                    rendered = " Teorema",
+                    highlight = "RenderMarkdownWarn",
+                    category = "obsidian",
+                },
+                osservazione = {
+                    raw = "[!OSS]",
+                    rendered = " Osservazione",
+                    highlight = "RenderMarkdownWarn",
+                    category = "obsidian",
+                },
+
+                note = {
+                    raw = "[!NOTE]",
+                    rendered = "󰋽 Note",
+                    highlight = "RenderMarkdownInfo",
+                    category = "github",
+                },
+                tip = {
+                    raw = "[!TIP]",
+                    rendered = "󰌶 Tip",
+                    highlight = "RenderMarkdownSuccess",
+                    category = "github",
+                },
+                important = {
+                    raw = "[!IMPORTANT]",
+                    rendered = "󰅾 Important",
+                    highlight = "RenderMarkdownHint",
+                    category = "github",
+                },
+                warning = {
+                    raw = "[!WARNING]",
+                    rendered = "󰀪 Warning",
+                    highlight = "RenderMarkdownWarn",
+                    category = "github",
+                },
+                caution = {
+                    raw = "[!CAUTION]",
+                    rendered = "󰳦 Caution",
+                    highlight = "RenderMarkdownError",
+                    category = "github",
+                },
+                abstract = {
+                    raw = "[!ABSTRACT]",
+                    rendered = "󰨸 Abstract",
+                    highlight = "RenderMarkdownInfo",
+                    category = "obsidian",
+                },
+                summary = {
+                    raw = "[!SUMMARY]",
+                    rendered = "󰨸 Summary",
+                    highlight = "RenderMarkdownInfo",
+                    category = "obsidian",
+                },
+                tldr = {
+                    raw = "[!TLDR]",
+                    rendered = "󰨸 Tldr",
+                    highlight = "RenderMarkdownInfo",
+                    category = "obsidian",
+                },
+                info = {
+                    raw = "[!INFO]",
+                    rendered = "󰋽 Info",
+                    highlight = "RenderMarkdownInfo",
+                    category = "obsidian",
+                },
+                todo = {
+                    raw = "[!TODO]",
+                    rendered = "󰗡 Todo",
+                    highlight = "RenderMarkdownInfo",
+                    category = "obsidian",
+                },
+                hint = {
+                    raw = "[!HINT]",
+                    rendered = "󰌶 Hint",
+                    highlight = "RenderMarkdownSuccess",
+                    category = "obsidian",
+                },
+                success = {
+                    raw = "[!SUCCESS]",
+                    rendered = "󰄬 Success",
+                    highlight = "RenderMarkdownSuccess",
+                    category = "obsidian",
+                },
+                check = {
+                    raw = "[!CHECK]",
+                    rendered = "󰄬 Check",
+                    highlight = "RenderMarkdownSuccess",
+                    category = "obsidian",
+                },
+                done = {
+                    raw = "[!DONE]",
+                    rendered = "󰄬 Done",
+                    highlight = "RenderMarkdownSuccess",
+                    category = "obsidian",
+                },
+                question = {
+                    raw = "[!QUESTION]",
+                    rendered = "󰘥 Question",
+                    highlight = "RenderMarkdownWarn",
+                    category = "obsidian",
+                },
+                help = {
+                    raw = "[!HELP]",
+                    rendered = "󰘥 Help",
+                    highlight = "RenderMarkdownWarn",
+                    category = "obsidian",
+                },
+                faq = {
+                    raw = "[!FAQ]",
+                    rendered = "󰘥 Faq",
+                    highlight = "RenderMarkdownWarn",
+                    category = "obsidian",
+                },
+                attention = {
+                    raw = "[!ATTENTION]",
+                    rendered = "󰀪 Attention",
+                    highlight = "RenderMarkdownWarn",
+                    category = "obsidian",
+                },
+                failure = {
+                    raw = "[!FAILURE]",
+                    rendered = "󰅖 Failure",
+                    highlight = "RenderMarkdownError",
+                    category = "obsidian",
+                },
+                fail = {
+                    raw = "[!FAIL]",
+                    rendered = "󰅖 Fail",
+                    highlight = "RenderMarkdownError",
+                    category = "obsidian",
+                },
+                missing = {
+                    raw = "[!MISSING]",
+                    rendered = "󰅖 Missing",
+                    highlight = "RenderMarkdownError",
+                    category = "obsidian",
+                },
+                danger = {
+                    raw = "[!DANGER]",
+                    rendered = "󱐌 Danger",
+                    highlight = "RenderMarkdownError",
+                    category = "obsidian",
+                },
+                error = {
+                    raw = "[!ERROR]",
+                    rendered = "󱐌 Error",
+                    highlight = "RenderMarkdownError",
+                    category = "obsidian",
+                },
+                bug = {
+                    raw = "[!BUG]",
+                    rendered = "󰨰 Bug",
+                    highlight = "RenderMarkdownError",
+                    category = "obsidian",
+                },
+                example = {
+                    raw = "[!EXAMPLE]",
+                    rendered = "󰉹 Example",
+                    highlight = "RenderMarkdownHint",
+                    category = "obsidian",
+                },
+                quote = {
+                    raw = "[!QUOTE]",
+                    rendered = "󱆨 Quote",
+                    highlight = "RenderMarkdownQuote",
+                    category = "obsidian",
+                },
+                cite = {
+                    raw = "[!CITE]",
+                    rendered = "󱆨 Cite",
+                    highlight = "RenderMarkdownQuote",
+                    category = "obsidian",
+                },
+            },
+            latex = { enabled = false },
+        }
+    end,
+}
--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,176 +1,81 @@
 return {
-    {
-        "obsidian-nvim/obsidian.nvim",
-        version = "*", -- recommended, use latest release instead of latest commit
-        lazy = true,
-        ft = "markdown",
-        -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-        -- event = {
-        --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-        --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-        --   -- refer to `:h file-pattern` for more examples
-        --   "BufReadPre path/to/my-vault/*.md",
-        --   "BufNewFile path/to/my-vault/*.md",
-        -- },
-        dependencies = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
-            "saghen/blink.cmp",
-        },
-        opts = {
-            legacy_commands = false,
-            workspaces = {
-                {
-                    name = "Appunti",
-                    path = "~/Documents/Appunti/",
-                },
-                -- {
-                --     name = "Personal",
-                --     path = "~/Documents/Personal-Vault/",
-                -- },
-                {
-                    name = "no-vault",
-                    path = function()
-                        -- alternatively use the CWD:
-                        -- return assert(vim.fn.getcwd())
-                        return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
-                    end,
-                    overrides = {
-                        notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
-                        new_notes_location = "current_dir",
-                        templates = {
-                            folder = vim.NIL,
-                        },
-                        disable_frontmatter = true,
-                    },
-                },
-            },
-            picker = {
-                -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
-                name = "snacks.pick",
-                -- Optional, configure key mappings for the picker. These are the defaults.
-                -- Not all pickers support all mappings.
-                note_mappings = {
-                    -- Create a new note from your query.
-                    new = "<C-x>",
-                    -- Insert a link to the selected note.
-                    insert_link = "<C-l>",
-                },
-                tag_mappings = {
-                    -- Add tag(s) to current note.
-                    tag_note = "<C-x>",
-                    -- Insert a tag at the current location.
-                    insert_tag = "<C-l>",
-                },
-            },
-            completion = { blink = true, nvim_cmp = false },
-
-            preferred_link_style = "markdown",
-
-            ui = {
-                enable = false, -- set to false to disable all additional syntax features
-                update_debounce = 200, -- update delay after a text change (in milliseconds)
-                max_file_length = 5000, -- disable UI features for files with more than this many lines
-                -- checkboxes = {
-                --     [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
-                --     ["x"] = { char = "", hl_group = "ObsidianDone" },
-                --     [">"] = { char = "", hl_group = "ObsidianRightArrow" },
-                --     ["+"] = { char = "", hl_group = "ObsidianRightArrow" },
-                --     ["-"] = { char = "", hl_group = "ObsidianTilde" },
-                --     ["~"] = { char = "", hl_group = "ObsidianTilde" },
-                --     ["!"] = { char = "", hl_group = "ObsidianImportant" },
-                -- },
-            },
-            -- Define how various check-boxes are displayed
-            checkbox = {
-                enabled = false,
-                create_new = false,
-                order = { " ", "x", ">", "+", "-", "~", "!" },
-            },
-
-            disable_frontmatter = true,
-
-            note_frontmatter_func = function(note)
-                -- Add the title of the note as an alias.
-                if note.title then
-                    note:add_alias(note.title)
-                end
-
-                local out = { id = note.id, tags = note.tags }
-
-                -- `note.metadata` contains any manually added fields in the frontmatter.
-                -- So here we just make sure those fields are kept in the frontmatter.
-                if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-                    for k, v in pairs(note.metadata) do
-                        out[k] = v
-                    end
-                end
-
-                return out
-            end,
-        },
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    dependencies = {
+        -- Required.
+        "nvim-lua/plenary.nvim",
+        "saghen/blink.cmp",
     },
-    -- {
-    --     "MeanderingProgrammer/render-markdown.nvim",
-    --     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    --     config = function()
-    --         require("render-markdown").setup {
-    --             latex = {
-    --                 enabled = true,
-    --                 render_modes = false,
-    --                 converter = { "utftex", "latex2text" },
-    --                 highlight = "RenderMarkdownMath",
-    --                 position = "center",
-    --                 top_pad = 0,
-    --                 bottom_pad = 0,
-    --             },
-    --         }
-    --     end,
-    -- },
-    -- {
-    --     "OXY2DEV/markview.nvim",
-    --     lazy = false,
-    --     dependencies = {
-    --         "saghen/blink.cmp",
-    --     },
-    --     config = function()
-    --         local presets = require "markview.presets"
-    --         require("markview").setup {
-    --             markdown = {
-    --                 headings = presets.headings.glow,
-    --                 horizontal_rules = presets.horizontal_rules.thick,
-    --                 tables = presets.tables.single,
-    --             },
-    --         }
-    --     end,
-    -- },
-    -- {
-    --     "jbyuki/nabla.nvim",
-    --     dependencies = {
-    --         "williamboman/mason.nvim",
-    --     },
-    --     lazy = false,
-    --
-    --     config = function() end,
-    --
-    --     keys = function()
-    --         return {
-    --             {
-    --                 "<leader>np",
-    --                 ':lua require("nabla").popup()<cr>',
-    --                 desc = "NablaPopUp",
-    --             },
-    --             {
-    --                 "<leader>nt",
-    --                 ':lua require("nabla").toggle_virt()<cr>',
-    --                 desc = "NablaToggle",
-    --             },
-    --             {
-    --                 "<leader>nr",
-    --                 ':lua require("nabla").enable_virt()<cr>',
-    --                 desc = "NablaRefresh",
-    --             },
-    --         }
-    --     end,
-    -- },
+    opts = {
+        legacy_commands = false,
+        workspaces = {
+            {
+                name = "Appunti",
+                path = "~/Documents/Appunti/",
+            },
+            {
+                name = "no-vault",
+                path = function()
+                    -- alternatively use the CWD:
+                    -- return assert(vim.fn.getcwd())
+                    return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+                end,
+                overrides = {
+                    notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
+                    new_notes_location = "current_dir",
+                    templates = {
+                        folder = vim.NIL,
+                    },
+                    disable_frontmatter = true,
+                },
+            },
+        },
+        picker = {
+            -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+            name = "snacks.pick",
+            -- Optional, configure key mappings for the picker. These are the defaults.
+            -- Not all pickers support all mappings.
+            note_mappings = {
+                -- Create a new note from your query.
+                new = "<C-x>",
+                -- Insert a link to the selected note.
+                insert_link = "<C-l>",
+            },
+            tag_mappings = {
+                -- Add tag(s) to current note.
+                tag_note = "<C-x>",
+                -- Insert a tag at the current location.
+                insert_tag = "<C-l>",
+            },
+        },
+        completion = { blink = true, nvim_cmp = false },
+
+        preferred_link_style = "markdown",
+
+        ui = {
+            enable = false, -- set to false to disable all additional syntax features
+            update_debounce = 200, -- update delay after a text change (in milliseconds)
+            max_file_length = 5000, -- disable UI features for files with more than this many lines
+        },
+        -- Define how various check-boxes are displayed
+        checkbox = {
+            enabled = false,
+            create_new = false,
+            order = { " ", "x", ">", "~", "!" },
+        },
+
+        frontmatter = {
+            enabled = false,
+        }
+    },
 }
