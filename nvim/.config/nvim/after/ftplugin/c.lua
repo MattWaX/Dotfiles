d6875f43d1a1c9f939733c5b850dda24297acad9
MattWaX <mattwexx2929@gmail.com>
2025-10-28 12:37:40 +0100

    nvim: multiple parameters when compiling from nvim

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -10,6 +10,6 @@ opt.shiftwidth = 8
 opt.makeprg = "make"
 
 map("n", "<F5>", ":Start -wait=always make -s run<CR>", { desc = "Build and run the c project"})
-map("n", "<F6>", ":Start -wait=always make -s run args=", { desc = "Build and run the c project with args"})
+map("n", "<F6>", ":Start -wait=always make -s run args=\"\"<Left>", { desc = "Build and run the c project with args"})
 map("n", "<F7>", ":Make build<CR>", { desc = "Build the c project"})
 map("n", "<F8>", ":Start -wait=always make -s debug<CR>", { desc = "Open the gdb debugger for the current c project"})
