010efa7f4d11c728b12c2d14e96d451688db89ee
MattWaX <mattwexx2929@gmail.com>
2025-11-11 19:46:00 +0100

    nvim: some fix up

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -41,8 +41,8 @@ return {
                 ["<C-y>"] = { "accept", "fallback" },
                 -- ["<CR>"] = { "accept", "fallback" },
 
-                ["<Tab>"] = { "select_prev", "fallback_to_mappings" },
-                ["<S-Tab>"] = { "select_next", "fallback_to_mappings" },
+                -- ["<S-Tab>"] = { "select_prev", "fallback_to_mappings" },
+                -- ["<Tab>"] = { "select_next", "fallback_to_mappings" },
                 ["<Up>"] = { "select_prev", "fallback" },
                 ["<Down>"] = { "select_next", "fallback" },
                 ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
--- nvim/.config/nvim/lua/plugins/whick-key.lua
+++ nvim/.config/nvim/lua/plugins/whick-key.lua
@@ -2,10 +2,11 @@ return {
     {
         "folke/which-key.nvim",
         lazy = false;
-        keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
         cmd = "WhichKey",
         opts = function()
-            return {}
+            return {
+                preset = "helix",
+            }
         end,
     },
 }
