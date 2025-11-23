fbf14493ac53626e7b3c7b8bf171696704e680c5
MattWaX <mattwexx2929@gmail.com>
2025-11-23 20:03:52 +0100

    scripts: songinfo fixed

--- scripts/.scripts/songinfo.sh
+++ scripts/.scripts/songinfo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 music_dir="$HOME/Music"
-filename="$(mpc --format "$music_dir"/%file% current)"
-previewname="/tmp/current_cover.jpg"
+current_song="$(mpc --format "$music_dir"/%file% current)"
+previewname="${current_song%/*}/cover.jpg"
+appname="mpd"
 
-yes "N" | ffmpeg -i "$filename" -map 0:1 "$previewname" > /dev/null 2>&1
+# yes "y" | ffmpeg -i "$filename" -map 0:1 "$previewname" # > /dev/null 2>&1
 previus_notification=$(cat /tmp/last-song-notification-id)
 if [ -z "$previus_notification" ]; then
-
-    notify-send "Now Playing" "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname" -p > /tmp/last-song-notification-id
+    notify-send -a $appname "Now Playing" "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname" -p > /tmp/last-song-notification-id
 else
-    notify-send -r "$previus_notification" "Now Playing" "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname" -p > /tmp/last-song-notification-id
+    notify-send -a $appname -r "$previus_notification" "Now Playing" "$(mpc --format '%title% \n%artist% - %album%' current)" -i "$previewname" -p > /tmp/last-song-notification-id
 fi
