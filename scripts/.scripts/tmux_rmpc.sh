897ad5f64a8cb8a7453ba384cb74b42821729225
MattWaX <mattwexx2929@gmail.com>
2025-11-27 18:24:18 +0100

    river: gracefull logout (with ly and uwsm)

--- scripts/.scripts/tmux_rmpc.sh
+++ scripts/.scripts/tmux_rmpc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-tmux new -s "mpc" -d 'while true; do rmpc; done' \; split-window -d 'cava' \; attach
+tmux new -As "mpc" -d 'while true; do rmpc; done' \; split-window -d 'cava' \; attach
