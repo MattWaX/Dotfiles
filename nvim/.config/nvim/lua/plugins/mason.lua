6f0b4f28598297fc61e4892875b952a4682a3b26
MattWaX <mattwexx2929@gmail.com>
2025-12-05 15:32:28 +0100

    nvim: no default lsp download

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -15,55 +15,55 @@ return {
             max_concurrent_installers = 10,
         },
     },
-    {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        opts = {
-            ensure_installed = {
-                -- lua
-                "lua-language-server",
-                "stylua",
-                -- shell
-                "bash-language-server",
-                "bash-debug-adapter",
-                "shfmt",
-                -- web
-                "css-lsp",
-                "html-lsp",
-                -- c/cpp
-                "clangd",
-                "clang-format",
-                -- rust
-                "rust-analyzer",
-                "taplo",
-                -- c, cpp, rust debugger
-                "codelldb",
-                -- asm
-                "asm-lsp",
-                "asmfmt",
-                -- java
-                "jdtls",
-                "java-debug-adapter",
-                -- php
-                "phpactor",
-                "php-debug-adapter",
-                -- tex
-                "texlab",
-                "bibtex-tidy",
-                -- R
-                -- "r-languageserver",
-                "air",
-                -- XML
-                "lemminx",
-                "xmlformatter",
-                -- treesitter
-                "tree-sitter-cli",
-            },
-
-            integrations = {
-                ["mason-lspconfig"] = true,
-                ["mason-null-ls"] = true,
-                ["mason-nvim-dap"] = true,
-            },
-        },
-    },
+    -- {
+    --     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    --     opts = {
+    --         ensure_installed = {
+    --             -- lua
+    --             "lua-language-server",
+    --             "stylua",
+    --             -- shell
+    --             "bash-language-server",
+    --             "bash-debug-adapter",
+    --             "shfmt",
+    --             -- web
+    --             "css-lsp",
+    --             "html-lsp",
+    --             -- c/cpp
+    --             "clangd",
+    --             "clang-format",
+    --             -- rust
+    --             "rust-analyzer",
+    --             "taplo",
+    --             -- c, cpp, rust debugger
+    --             "codelldb",
+    --             -- asm
+    --             "asm-lsp",
+    --             "asmfmt",
+    --             -- java
+    --             "jdtls",
+    --             "java-debug-adapter",
+    --             -- php
+    --             "phpactor",
+    --             "php-debug-adapter",
+    --             -- tex
+    --             "texlab",
+    --             "bibtex-tidy",
+    --             -- R
+    --             -- "r-languageserver",
+    --             "air",
+    --             -- XML
+    --             "lemminx",
+    --             "xmlformatter",
+    --             -- treesitter
+    --             "tree-sitter-cli",
+    --         },
+    --
+    --         integrations = {
+    --             ["mason-lspconfig"] = true,
+    --             ["mason-null-ls"] = true,
+    --             ["mason-nvim-dap"] = true,
+    --         },
+    --     },
+    -- },
 }
