8060b657feef25dc5326f78d5ea5f0ae37b6d4c6
MattWaX <mattwexx2929@gmail.com>
2026-02-03 14:35:48 +0100

    nvim: qol typst

--- nvim/.config/nvim/after/ftplugin/typst.lua
+++ nvim/.config/nvim/after/ftplugin/typst.lua
@@ -3,6 +3,14 @@ require "options"
 local opt = vim.o
 local map = vim.keymap.set
 
+opt.expandtab = true
+opt.smartindent = true
+opt.tabstop = 8
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.textwidth = 79
+opt.colorcolumn = "+1"
+
 opt.makeprg = "typst"
 
 map("n", "m!", "<CMD>Make! watch %:p<CR>")
