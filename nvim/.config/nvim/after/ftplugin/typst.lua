bec7c7aa2046668ac58efea02aa5452989a10bad
MattWaX <mattwexx2929@gmail.com>
2026-03-01 16:54:10 +0100

    nvim: typst support

--- nvim/.config/nvim/after/ftplugin/typst.lua
+++ nvim/.config/nvim/after/ftplugin/typst.lua
@@ -3,16 +3,11 @@ require "options"
 local opt = vim.o
 local map = vim.keymap.set
 
-opt.expandtab = true
-opt.smartindent = true
-opt.tabstop = 8
-opt.softtabstop = 4
-opt.shiftwidth = 4
 opt.textwidth = 79
 opt.colorcolumn = "+1"
 
-opt.makeprg = "typst"
+opt.makeprg = "just"
 
-map("n", "m!", "<CMD>Make! watch %:p<CR>")
-map("n", "m<CR>", "<CMD>Make! c %:p<CR>")
+map("n", "m!", "<CMD>Make watch<CR>")
+map("n", "m<CR>", "<CMD>Make! compile<CR>")
 map("n", "gz", "<CMD>Make! c %:p<CR><CMD>silent !zathura --fork %:p:r.pdf &<CR>", { desc = "Open the current typst file in zathura"})
---
+++ nvim/.config/nvim/after/indent/typst.lua
@@ -0,0 +1,9 @@
+require "options"
+
+local opt = vim.o
+
+opt.expandtab = true
+opt.smartindent = true
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
