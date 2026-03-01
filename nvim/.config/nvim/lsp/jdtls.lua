2d0efc3f4096e0a56813f0bb058e0d9077e4cc3a
MattWaX <mattwexx2929@gmail.com>
2026-03-01 16:54:37 +0100

    nvim: lsp setups

--- nvim/.config/nvim/lsp/jdtls.lua
+++ nvim/.config/nvim/lsp/jdtls.lua
@@ -1,7 +1,20 @@
 ---@type vim.lsp.Config
 return {
     filetypes = { "java" },
-    cmd = { "jdtls" },
-    root_markers = { ".git" },
+    cmd = {
+        "jdtls",
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xms1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+    },
+    root_markers = { ".git", "gradlew" },
     settings = {},
 }
--- nvim/.config/nvim/lsp/tinymist.lua
+++ nvim/.config/nvim/lsp/tinymist.lua
@@ -2,5 +2,16 @@
 return {
     cmd = { "tinymist" },
     filetypes = { "typst", "typ" },
+    settings = {
+        formatterMode = "typstyle", -- or "typstfmt"
+        formatterProseWrap = true, -- wrap lines in content mode
+        formatterPrintWidth = 80, -- limit line length to 80 if possible
+        formatterIndentSize = 4, -- indentation width
+        syntaxOnly = "enabled",
+        lint = {
+            enabled = true,
+            when = "onType",
+        },
+    },
     root_markers = { ".git" },
 }
