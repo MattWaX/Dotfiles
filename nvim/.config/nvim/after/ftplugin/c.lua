3f1ccc0b3b61d0df5a78abf544b3f54ab0243599
MattWaX <mattwexx2929@gmail.com>
2026-04-08 21:33:33 +0200

    nvim: better file support

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -3,11 +3,15 @@ require "options"
 local opt = vim.o
 local map = vim.keymap.set
 
-opt.tabstop = 8
-opt.softtabstop = 8
-opt.shiftwidth = 8
+-- opt.tabstop = 8
+-- opt.softtabstop = 8
+-- opt.shiftwidth = 8
+
+opt.modeline = false
 
 opt.makeprg = "make"
+
+map("n", "m<CR>", ":Make run<CR>", { desc = "Build and run the c project"})
 
 map("n", "<F5>", ":Start -wait=always make -s run<CR>", { desc = "Build and run the c project"})
 map("n", "<F6>", ":Start -wait=always make -s run args=\"\"<Left>", { desc = "Build and run the c project with args"})
