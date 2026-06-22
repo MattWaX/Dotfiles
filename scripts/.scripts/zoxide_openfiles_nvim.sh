a11c1b3f61e6fddf28ac5d3ef6c328b7df952297
MattWaX <mattwexx2929@gmail.com>
2026-06-22 15:29:01 +0200

    scripts: fixes

--- scripts/.scripts/zoxide_openfiles_nvim.sh
+++ scripts/.scripts/zoxide_openfiles_nvim.sh
@@ -5,13 +5,13 @@
 search_with_zoxdie() {
     if [ -z "$1" ]; then
         # use fd with fzf to select & open a file when no arg are provided
-        file="$(fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup | fzf --height=70% --preview='bat -n --color=always --line-range :500 {}')"
+        file="$(fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup | sk --height=70% --preview='bat -n --color=always --line-range :500 {}')"
         if [ -n "$file" ]; then
             nvim "$file"
         fi
     else
         # Handle when an arg is provided
-        lines=$(zoxide query -l | xargs -I {} fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup -E .vscode "$1" {} | fzf --no-sort) # Initial filter attempt with fzf
+        lines=$(zoxide query -l | xargs -I {} fd --type f -I -H -E .git -E .git-crypt -E .cache -E .backup -E .vscode "$1" {} | sk --no-sort) # Initial filter attempt with fzf
         line_count="$(echo "$lines" | wc -l | xargs)" # Trim any leading spaces
 
         if [ -n "$lines" ] && [ "$line_count" -eq 1 ]; then
@@ -20,7 +20,7 @@ search_with_zoxdie() {
             nvim "$file"
         elif [ -n "$lines" ]; then
             # If multiple files are found, allow further selection using fzf and bat for preview
-            file=$(echo "$lines" | fzf --query="$1" --height=70% --preview='bat -n --color=always --line-range :500 {}')
+            file=$(echo "$lines" | sk --query="$1" --height=70% --preview='bat -n --color=always --line-range :500 {}')
             if [ -n "$file" ]; then
                 nvim "$file"
             fi
